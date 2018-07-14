42b17f42869fd1a935bec7cf13b3728ccf9c2824
Sasha Bajzek <dabridginator@gmail.com>
2018-07-14 11:29:41 -0700

    Add tests for search behavior

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { shallow } from "enzyme";
+import preload from "../../data.json";
 import Search from "../Search";
+import ShowCard from "../ShowCard";
 
 // test is run in node, so make sure this is added to babelrc
 // "env": {
@@ -14,7 +16,29 @@ import Search from "../Search";
 // console.log(process.env.NODE_ENV);
 // outputs "test"
 
+// if there are any tests I don't want to run, put an x in front to skip it.  xdescribe, xit, xtest
+
 test("Search renders correctly", () => {
   const component = shallow(<Search />);
   expect(component).toMatchSnapshot();
+});
+
+test("Search should render correct amount of shows", () => {
+  const component = shallow(<Search />);
+  // enzyme can find a component based on the react component, ShowCard
+  expect(component.find(ShowCard).length).toEqual(preload.shows.length);
+});
+
+test("Search should render correct amount of shows based on search term", () => {
+  const searchWord = "black";
+  const component = shallow(<Search />);
+  // enzyme can also find a component based on css selectors, such as input
+  component.find("input").simulate("change", { target: { value: searchWord } });
+  const showCount = preload.shows.filter(
+    show =>
+      `${show.title} ${show.description}`
+        .toUpperCase()
+        .indexOf(searchWord.toUpperCase()) >= 0
+  ).length;
+  expect(component.find(ShowCard).length).toEqual(showCount);
 });
