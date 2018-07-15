07f7e436f9de8535d91b0a689ebe72bd7e648702
Sasha Bajzek <dabridginator@gmail.com>
2018-07-15 15:27:35 -0700

    Add combineReducers

--- js/reducers.js
+++ js/reducers.js
@@ -1,23 +1,17 @@
 // @flow
 
+import { combineReducers } from "redux";
 import { SET_SEARCH_TERM } from "./actions";
 
-const DEFAULT_STATE = {
-  searchTerm: ""
-};
-
-// setSearchTerm is a reducer
-// anything that takes in state and action and returns state is a reducer
-const setSearchTerm = (state, action) =>
-  Object.assign({}, state, { searchTerm: action.payload });
-
-const rootReducer = (state = DEFAULT_STATE, action) => {
-  switch (action.type) {
-    case SET_SEARCH_TERM:
-      return setSearchTerm(state, action);
-    default:
-      return state;
+// this reducer only worries about the searchTerm part of the state
+const searchTerm = (state = "", action: Action) => {
+  if (action.type === SET_SEARCH_TERM) {
+    return action.payload;
   }
+  return state;
 };
+
+// there is a piece of state called searchTerm and the reducer for it is called searchTerm, so {searchTerm: searchTerm}, but because of es6, can just write {searchTerm}
+const rootReducer = combineReducers({ searchTerm });
 
 export default rootReducer;
