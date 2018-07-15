b5014784cb38540ffcc4e9d6cd6d628027c76d7e
Sasha Bajzek <dabridginator@gmail.com>
2018-07-15 15:28:43 -0700

    Type actionCreators

--- js/actionCreators.js
+++ js/actionCreators.js
@@ -2,7 +2,7 @@
 
 import { SET_SEARCH_TERM } from "./actions";
 
-export function setSearchTerm(searchTerm) {
+export function setSearchTerm(searchTerm: string) {
   return {
     type: SET_SEARCH_TERM,
     payload: searchTerm
