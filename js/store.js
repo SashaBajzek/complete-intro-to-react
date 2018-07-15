8d61638bfa404c654292831bec756bdb726d486d
Sasha Bajzek <dabridginator@gmail.com>
2018-07-15 15:39:30 -0700

    Add redux devTools

--- js/store.js
+++ js/store.js
@@ -1,8 +1,18 @@
 // @flow
 
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 import reducer from "./reducers";
 
-const store = createStore(reducer);
+// if typeof window === "object", aka if in the browser, so it will still run in node for ssr
+// function that returns nothing aka identity function f => f
+const store = createStore(
+  reducer,
+  compose(
+    typeof window === "object" &&
+      typeof window.devToolsExtension !== "undefined"
+      ? window.devToolsExtension()
+      : f => f
+  )
+);
 
 export default store;
